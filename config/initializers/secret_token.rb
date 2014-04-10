75457dc1176168163f11ef04c6c6ab306756b506
Josh Klein <kleincomplex@gmail.com>
2014-04-09 23:12:16 -0500

    Initialize repo on vb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'fc88672dde2733284760f180ddff1affd0e02ba38f85f486f25fa20814d4264f72e0ce0928fe22e9c0a9706ef443699ca94c13c552d94cec4cc6e04f0a0e925d'
